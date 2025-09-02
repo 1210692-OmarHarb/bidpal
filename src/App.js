7320f44083992cc38b7b42dce26fee8d852cd960
1210692-OmarHarb <1210692@student.birzeit.edu>
2025-09-02 14:30:16 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import nav from "./components/nav";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      sdsds <nav />
+    </div>
+  );
 }
 
 export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,1 +1,7 @@
-rfce;
+import React from "react";
+
+function nav() {
+  return <div>navsdsdssdsdsdd</div>;
+}
+
+export default nav;
